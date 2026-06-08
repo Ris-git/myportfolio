ff9ca1ca0d545e080e891a1e863d3a13147bb7e6
PythonHacker24 <adityapatil24680@gmail.com>
2026-06-08 17:05:33 +0530

    updated for vercel insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import { ThemeProvider } from "./providers";
 
@@ -27,6 +28,7 @@ export default function RootLayout({
         <ThemeProvider>
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
